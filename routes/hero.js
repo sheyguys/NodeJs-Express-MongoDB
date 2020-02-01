797a3025e01dba0ddcdea29fbdea7587e55480a4
sheyguys <sameyu.ping@gmail.com>
2020-02-01 22:05:19 +0700

    Add Express Validator

--- routes/hero.js
+++ routes/hero.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 /* GET user listing. */
 router.get('/', function(req, res, next) {
@@ -12,12 +13,20 @@ router.get('/add', function(req, res, next) {
 });
 
 /* Add user */
-router.post('/add', function(req, res, next) {
-  console.log(req.body.name)
-  console.log(req.body.agi)
-  console.log(req.body.str)
-  console.log(req.body.int)
-  console.log(req.body.description)
+router.post('/add',[
+  check("name","Please input your Hero Name.").not().isEmpty(),
+  check("agi","Please input your Hero Agility.").not().isEmpty(),
+  check("str","Please input your Hero Strength.").not().isEmpty(),
+  check("int","Please input your Hero Intelligence.").not().isEmpty(),
+  check("description","Please input your Hero Description.").not().isEmpty()
+], function(req, res, next) {
+  const response = validationResult(req);
+  var errors = response.errors
+  if (!errors.isEmpty()) {
+    res.render('addhero',{errors:errors});
+  } else {
+    //insert data to db
+  }
 });
 
 module.exports = router;
